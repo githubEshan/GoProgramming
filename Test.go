1df28a71325bcb44deacda7613f57de0b9711a3a
Eshan <erdegamwar@gmail.com>
2023-11-11 18:34:37 -0500

    variables in go

--- Test.go
+++ Test.go
@@ -2,8 +2,9 @@ package main
 
 import "fmt"
 
-func main() {
-
-	fmt.Println("Hello Go World!")
-
+func varaibles() {
+	age := "10"
+	//works only with blockscope
+	// := sign tells go we are creating one or more new variables
+	fmt.Println("hello", age)
 }

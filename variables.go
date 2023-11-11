0292374a05710addcc74a41ba968a11e17efb728
Eshan <erdegamwar@gmail.com>
2023-11-11 18:46:22 -0500

    fixed file name

--- variables.go
+++ variables.go
@@ -6,5 +6,6 @@ func varaibles() {
 	age := "10"
 	//works only with blockscope
 	// := sign tells go we are creating one or more new variables
+	//cannot use :- for already defined variables 
 	fmt.Println("hello", age)
 }
